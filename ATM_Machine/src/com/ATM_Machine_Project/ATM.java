ec57e10069bc119b9f7415b053bc25f92cd3d1e6
Karan24KT <74721766+Karan24KT@users.noreply.github.com>
2024-04-06 19:52:58 +0530

    Initial Commit

---
+++ ATM_Machine/src/com/ATM_Machine_Project/ATM.java
@@ -0,0 +1,117 @@
+package com.ATM_Machine_Project;
+
+import java.util.Scanner;
+
+//import javax.imageio.plugins.tiff.ExifGPSTagSet;
+
+public class ATM {
+	final int pin = 1234;
+	//public static float balance = 10002;
+	
+	public void menu() {
+		// 1. Withdraw Money
+		// 2. Account Balance
+		// 3. Deposit Money
+		// 4. Exit
+		System.out.println("***** ATM Services *****");
+		System.out.println();
+		System.out.println("Enter Your Choice");
+		System.out.println("1. Check Account Balance");
+		System.out.println("2. WithDraw Money");
+		System.out.println("3. Deposit Money");
+		System.out.println("4. EXIT");
+		
+		Scanner scanner = new Scanner(System.in);
+		int customerChoice = scanner.nextInt();
+		
+		checkCustomMenuChoice(customerChoice);
+		 	
+	}
+	
+	public void showMenuAgain() {
+		//showMenuAgain==true ? menu() : System.exit(0);
+		Scanner scanner = new Scanner(System.in);
+		int  showAgain = scanner.nextInt();
+		
+		if(showAgain == 1) {
+			menu();
+		}
+		else {
+			MenuFunction.exit();
+			//System.exit(0);
+		}
+	}
+	
+	public void checkCustomMenuChoice(int customerChoice) {
+		
+		switch (customerChoice) {
+		case 1:
+			System.out.println("### Current Account Balance Service ### ");
+			MenuFunction.checkBalance();
+			System.out.println();
+			System.out.println("Show Service Again -> Enter 1/0");
+			showMenuAgain();
+			break;
+		case 2:
+			System.out.println("### WithDraw Money Service ###");
+			MenuFunction.withdrawMoney();
+			System.out.println();
+			System.out.println("Show Service Again -> Enter 1/0");
+			System.out.println();
+			showMenuAgain();
+			break;
+		case 3:
+			System.out.println("### Deposit Money Service ###");
+			MenuFunction.depositMoney();
+			System.out.println();
+			System.out.println("### Show Services Again -> Enter 1/0 ###");
+			showMenuAgain();
+			break;
+		case 4:
+			MenuFunction.exit();
+			System.out.println("Customer Choose to Exit");
+			break;
+		default:
+			System.out.println("Invalid Customer Choice");
+			showMenuAgain();
+			break;
+		}
+		
+	}
+	
+	public void EnterPIN() {
+		System.out.println("Enter 4 Digit PIN");
+		Scanner scanner = new Scanner(System.in);
+		int PIN = scanner.nextInt();
+		
+		checkPIN(PIN);
+	}
+	
+	public void checkPIN(int pinEnterByCustomer) {
+		if(pinEnterByCustomer == pin) {
+			System.out.println("***** Access Granted *****");
+			System.out.println("Correct PIN");
+			System.out.println();
+			menu();
+		}
+		else {
+			System.out.println("***** Security Alert ******");
+			System.out.println("Invalid PIN");
+			System.out.println();
+			System.out.println("Please Enter PIN Again");
+			EnterPIN();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("***** WELCOME To KT~Dev ATM ******");
+		System.out.println();
+		ATM Customer1 = new ATM();
+		
+		//Customer1.pin = 1355;
+		Customer1.EnterPIN();
+		
+		 
+	}
+}
---
+++ ATM_Machine/src/com/ATM_Machine_Project/MenuFunction.java
@@ -0,0 +1,58 @@
+package com.ATM_Machine_Project;
+
+import java.util.Scanner;
+
+public class MenuFunction {
+	
+	int amount;
+	static int balance = 10000;
+	 
+	public static void checkBalance() {
+		System.out.println("+++ Current Balance = "+ balance);
+		System.out.println();
+		// System.out.println(balance);
+		//ATM.balance;
+	}
+	public static void withdrawMoney() {
+		System.out.println("Enter Amount to be WithDraw");
+		Scanner scanner = new Scanner(System.in);
+		float withdrawAmount = scanner.nextFloat();
+		
+		if(withdrawAmount>1 && withdrawAmount<balance) {
+			 balance -=   withdrawAmount;
+			System.out.println("Successfully withdraw amount "+withdrawAmount );
+			System.out.println("New Balance = " + balance);
+			System.out.println();
+		}
+		else {
+			System.out.println("Invalid Amount Entered or Low Balance");
+			System.out.println("Current Balance = "+ balance);
+			System.out.println();
+		}
+	}
+	public static void depositMoney() {
+		System.out.println("Enter Amount to be Deposited");
+		Scanner scanner = new Scanner(System.in);
+		float depositedAmount = scanner.nextFloat();
+		
+		if(depositedAmount>1) {
+			 balance  += depositedAmount;
+			System.out.println("Successfully Deposited amount "+depositedAmount);
+			System.out.println("New Balance = " + balance);
+			System.out.println();
+		}
+		else {
+			System.out.println("Invalid Amount Entered");
+			System.out.println("Current Balance = "+balance);
+			System.out.println();
+		}
+		//ATM.balance;
+	}
+	public static void exit() {
+		System.out.println("**** Thank You For Using Our ATM ******");
+		//ATM.balance;
+		System.exit(0);
+	}
+	
+	 
+}
