a7581727488299dce9788f0386c66706e31da202
Karan24KT <74721766+Karan24KT@users.noreply.github.com>
2024-04-07 00:17:10 +0530

    Updated Using Constructor MenuFunction.java

--- ATM_Machine/src/com/ATM_Machine_Project/MenuFunction.java
+++ ATM_Machine/src/com/ATM_Machine_Project/MenuFunction.java
@@ -3,56 +3,56 @@ package com.ATM_Machine_Project;
 import java.util.Scanner;
 
 public class MenuFunction {
-	
+
 	int amount;
 	static int balance = 10000;
-	 
+
 	public static void checkBalance() {
-		System.out.println("+++ Current Balance = "+ balance);
+		System.out.println("+++ Current Balance = " + balance);
 		System.out.println();
 		// System.out.println(balance);
-		//ATM.balance;
+		// ATM.balance;
 	}
+
 	public static void withdrawMoney() {
 		System.out.println("Enter Amount to be WithDraw");
 		Scanner scanner = new Scanner(System.in);
 		float withdrawAmount = scanner.nextFloat();
-		
-		if(withdrawAmount>1 && withdrawAmount<balance) {
-			 balance -=   withdrawAmount;
-			System.out.println("Successfully withdraw amount "+withdrawAmount );
+
+		if (withdrawAmount > 1 && withdrawAmount < balance) {
+			balance -= withdrawAmount;
+			System.out.println("Successfully withdraw amount " + withdrawAmount);
 			System.out.println("New Balance = " + balance);
 			System.out.println();
-		}
-		else {
+		} else {
 			System.out.println("Invalid Amount Entered or Low Balance");
-			System.out.println("Current Balance = "+ balance);
+			System.out.println("Current Balance = " + balance);
 			System.out.println();
 		}
 	}
+
 	public static void depositMoney() {
 		System.out.println("Enter Amount to be Deposited");
 		Scanner scanner = new Scanner(System.in);
 		float depositedAmount = scanner.nextFloat();
-		
-		if(depositedAmount>1) {
-			 balance  += depositedAmount;
-			System.out.println("Successfully Deposited amount "+depositedAmount);
+
+		if (depositedAmount > 1) {
+			balance += depositedAmount;
+			System.out.println("Successfully Deposited amount " + depositedAmount);
 			System.out.println("New Balance = " + balance);
 			System.out.println();
-		}
-		else {
+		} else {
 			System.out.println("Invalid Amount Entered");
-			System.out.println("Current Balance = "+balance);
+			System.out.println("Current Balance = " + balance);
 			System.out.println();
 		}
-		//ATM.balance;
+		// ATM.balance;
 	}
+
 	public static void exit() {
 		System.out.println("**** Thank You For Using Our ATM ******");
-		//ATM.balance;
+		// ATM.balance;
 		System.exit(0);
 	}
-	
-	 
+
 }
